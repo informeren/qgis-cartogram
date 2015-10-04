6e9b381cb4610bcca7f71d127c547f9af4867a13
wulff <wulff@ratatosk.net>
2015-10-04 19:23:28 +0200

    Updated Danish translation

--- i18n/da.ts
+++ i18n/da.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="da" sourcelanguage="en">
+<!DOCTYPE TS>
+<TS version="2.0" language="da" sourcelanguage="en">
 <context>
     <name>Cartogram</name>
     <message>
@@ -18,12 +19,12 @@
         <translation>Tilføj demo-lag</translation>
     </message>
     <message>
-        <location filename="cartogram.py" line="146"/>
+        <location filename="cartogram.py" line="147"/>
         <source>Cancel</source>
         <translation>Annullér</translation>
     </message>
     <message>
-        <location filename="cartogram.py" line="185"/>
+        <location filename="cartogram.py" line="187"/>
         <source>Cartogram creation cancelled by user.</source>
         <translation>Oprettelse af cartogram stoppet af brugeren.</translation>
     </message>
@@ -33,35 +34,45 @@
         <translation>Du skal bruge mindst ét vektorlag for at kunne oprette et cartogram.</translation>
     </message>
     <message>
-        <location filename="cartogram.py" line="198"/>
+        <location filename="cartogram.py" line="206"/>
         <source>Please select an input layer.</source>
         <translation>Du skal vælge et input-lag.</translation>
     </message>
     <message>
-        <location filename="cartogram.py" line="200"/>
+        <location filename="cartogram.py" line="208"/>
         <source>Please select an area field.</source>
         <translation>Du skal vælge et input-felt.</translation>
     </message>
+    <message>
+        <location filename="cartogram.py" line="192"/>
+        <source>An error ocurred during cartogram creation. Please see the &quot;Plugins&quot; log for details.</source>
+        <translation>Der opstod en fejl. Se venligst &quot;Plugin&quot;-loggen for flere detaljer.</translation>
+    </message>
+    <message>
+        <location filename="cartogram.py" line="229"/>
+        <source>One or more rows in your &quot;area&quot; column contain zero or NULL values. Do you want to continue anyway with modified (non-zero) values for those fields?</source>
+        <translation>En eller flere rækker i din &quot;felt&quot;-kolonne indeholder 0 eller NULL. Ønsker du at fortsætte med ændrede værdier for disse felter?</translation>
+    </message>
 </context>
 <context>
-    <name>CartogramDialogBase</name>
+    <name>CartogramDialog</name>
     <message>
-        <location filename="cartogram_dialog_base.ui" line="14"/>
+        <location filename="cartogram_dialog.ui" line="14"/>
         <source>Cartogram</source>
         <translation>Cartogram</translation>
     </message>
     <message>
-        <location filename="cartogram_dialog_base.ui" line="29"/>
+        <location filename="cartogram_dialog.ui" line="29"/>
         <source>Input layer:</source>
         <translation>Lag:</translation>
     </message>
     <message>
-        <location filename="cartogram_dialog_base.ui" line="39"/>
+        <location filename="cartogram_dialog.ui" line="39"/>
         <source>Area field:</source>
         <translation>Felt:</translation>
     </message>
     <message>
-        <location filename="cartogram_dialog_base.ui" line="49"/>
+        <location filename="cartogram_dialog.ui" line="49"/>
         <source>Number of iterations to perform:</source>
         <translation>Antal iterationer:</translation>
     </message>
